3d9bdd26c622dc48ce651fd2724b346a73f2b80c
Jethro Dean <jethroldean@gmail.com>
2022-02-24 14:36:07 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* code goes here */
-document.addEventListener('DOMContentLoaded', () => {
-    let e = document.querySelector("div#hidden-div");
-    let newVal = parseInt(e.innerText) + 1;
-    e.innerText = newVal
-  })
+document.addEventListener("DOMContentLoaded", () => {
+  let e = document.querySelector("div#hidden-div")
+  console.log(e.innerText)
+  e.innerText = parseInt(e.innerText) + 1
+})
+
--- script.js
+++ script.js
@@ -2,4 +2,3 @@ document.addEventListener("DOMContentLoaded", e => {
     document.querySelector("body")
     .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
-
