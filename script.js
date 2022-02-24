8d09b4ad49ecb2f5ce3780b7c28dcb5a85b174a2
Jethro Dean <jethroldean@gmail.com>
2022-02-24 14:24:00 +1100

    Done.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", e => {
     document.querySelector("body")
-     .addEventListener("click", e => console.log("Reggae, Reggae!"));
-  })
+    .addEventListener("click", e => console.log("Reggae, Reggae!"));
+})
+
